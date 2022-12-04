758f15e952569a2855c2334d6d71ed9b616cfd98
Shadowsong <ombrechanson@outlook.com>
2022-12-04 19:51:25 +0800

    响应式优化

---
+++ js/indexJS.js
@@ -0,0 +1,18 @@
+window.onload = () => {
+    setNav()
+    window.addEventListener('resize', setNav)
+}
+
+const menu = document.getElementsByClassName('nav-content-menu')
+
+const setMenu = () => {
+    const menuStyle = window.getComputedStyle(menu[0])
+    menu[0].style.display =
+        menuStyle.display === 'block' ? 'none' : 'block'
+}
+
+const setNav = () => {
+    menu[0].style.display =
+        eval(document.body.clientWidth) > eval(600) ? 'block' : 'none'
+}
+
